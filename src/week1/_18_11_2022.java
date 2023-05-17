4bc227bbd3bcb66611a913eaad860034e6785dba
ChatGTHB <cihat.kose@hotmail.com>
2023-05-17 20:52:36 +0300

    _18_11_2022

---
+++ src/week1/_18_11_2022.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class _18_11_2022 {
+    public static void main(String[] args) {
+        String givenString = "8 8 Bi fk8h B 8 BB8B B B B888 c hl8 BhB fd";
+
+        // Verilen String de bosluklari kaldirarak ekrana yeniden yazdiriniz...
+        // String methodlarindan uygun olanini kullaniniz...
+
+        System.out.println(givenString.replace(" ", ""));
+
+    }
+}
