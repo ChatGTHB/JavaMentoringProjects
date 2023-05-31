75c06c799fabe8c8bb39bb52f37dec346ede84d8
ChatGTHB <cihat.kose@hotmail.com>
2023-05-31 23:38:32 +0300

    week2 _S2_Arithmetic_Operators added

---
+++ src/week2/_S2_Arithmetic_Operators.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class _S2_Arithmetic_Operators {
+    public static void main(String[] args) {
+        //Aşağıdaki işlemlerin sonucunda konsol çıktısı ne olur?
+
+        System.out.println(10 + 20 + "Quiz");
+        System.out.println("Quiz" + 10 + 20);
+        System.out.println(10 * 20 + "Quiz");
+        System.out.println("Quiz" + 10 * 20);
+
+    }
+}
