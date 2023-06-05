ebea7231660180162176f4b3390221b1ee2d8230
ChatGTHB <cihat.kose@hotmail.com>
2023-06-05 17:49:37 +0300

    week3 _ForLoop added

---
+++ src/week3/_ForLoop.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class _ForLoop {
+    public static void main(String[] args) {
+
+        // 5 kez Merhaba Dünya yazdırınız
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Merhaba Dünya");
+        }
+
+        // 0'dan 100'e kadar olan çift sayıları yazdırınız.
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+
+
+    }
+}
